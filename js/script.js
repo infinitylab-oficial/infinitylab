281e18ceb77736b6f9370db70d1cf88c0710b239
Jaam367 <jhosmar.alvarado@tecsup.edu.pe>
2026-08-15 13:03:51 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -159,7 +159,7 @@ function comprarProducto(version) {
     let message = "";
 
     if (version === "escritorio") {
-        message = "Hola InfinityLab, me interesa adquirir el *Silence - Versión Escritorio* (S/ 109.00). ¿Cuáles son los pasos para realizar el envío?";
+        message = "Hola InfinityLab, me interesa adquirir el *Silence - Versión Escritorio* (S/ 99.00). ¿Cuáles son los pasos para realizar el envío?";
     } else if (version === "portatil") {
         message = "Hola InfinityLab, me interesa adquirir el *Silence - Versión Portátil Avanzado* (S/ 169.00). ¿Cuáles son los pasos para realizar el envío?";
     } else if (version === "portatil-pro") {
